045d3d7d35531bf405ebb7dccd5fce8d37ef2982
com.AlifKhosyi <alifkhosyi@gmail.com>
2022-01-04 21:04:21 +0800

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Alif Khosyi Rahmatullah on 04/01/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
